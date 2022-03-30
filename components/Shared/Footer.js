8c1e9a007d0e8b2379e59590fa897d17b03603e7
codergalib20 <coderboygalif@gmail.com>
2022-03-30 22:47:01 +0600

    commited commited

--- components/Shared/Footer.js
+++ components/Shared/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Footer = () => {
   return (
     <div>
-      home
+      
     </div>
   );
 };
 
-export default Home;
+export default Footer;
