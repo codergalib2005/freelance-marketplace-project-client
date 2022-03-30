42d3df97e49e405c5239acdba456d2519a73eb95
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-03-30 20:47:16 +0600

    new change

--- pages/details/index.js
+++ pages/details/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Details = () => {
-  return <div>details root</div>;
+  return (
+    <div>
+      details root
+      <h3>This is details root</h3>
+      <h3>This is details product name</h3>
+    </div>
+  );
 };
 
 export default Details;
