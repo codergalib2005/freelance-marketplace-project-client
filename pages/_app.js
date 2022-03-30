285449c9908d69fd0e0a021610805fea1a5ba93e
codergalib20 <coderboygalif@gmail.com>
2022-03-30 13:01:37 +0600

    commited

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-
+import 'antd/dist/antd.css';
+import '../styles/custom.css';
+import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
