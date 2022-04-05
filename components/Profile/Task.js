38acc1d18d6d8a285cbd71fe9cc8fd576b5a2f9b
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-04-06 02:21:16 +0600

    profile configed

--- components/Profile/Task.js
+++ components/Profile/Task.js
@@ -29,7 +29,7 @@ const Task = () => {
   return (
     <div className="my-12">
       <p>Here is all of the task items</p>
-      <div className="grid grid-cols-5 mt-6 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 mt-6 gap-4">
         <div className=" col-span-3 shadow-md rounded-xl p-1 bg-white">
           <div className="flex justify-between p-4 bg-[#F9FBFC]">
             <p className="text-gray-500 text-sm">Gigs</p>
@@ -45,6 +45,7 @@ const Task = () => {
                   <p className="font-bold text-gray-800 text-md">{arr.title}</p>
                   <p className="text-xs text-gray-300">{arr.subtitle}</p>
                 </div>
+
                 <div className="">
                   <p className="text-gray-700 font-semibold">${arr.price}</p>
                 </div>
@@ -54,7 +55,15 @@ const Task = () => {
         </div>
 
         {/*  */}
-        <div className="">a</div>
+        <div className="col-span-2 shadow-md rounded-xl px-6 py-2 h-48 bg-white flex gap-6 items-center">
+          <div className="">
+            <img src="/profile/pie-chart.png" alt="" />
+            <p className="text-gray-700 text-2xl font-semibold">36.9k</p>
+          </div>
+          <div className="w-6/12 mx-auto">
+            <img src="/profile/Business-failure-pana.png" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
