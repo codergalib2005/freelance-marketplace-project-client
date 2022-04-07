03efccde58140df6e825666db3585500661d5e33
unknown <aarnob2020@gmail.com>
2022-04-07 14:05:07 +0600

    something change

--- components/Home/HowItWorks.js
+++ components/Home/HowItWorks.js
@@ -28,7 +28,7 @@ const HowItWorks = () => {
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat rerum sequi dicta, praesentium ipsa dolores illum quia voluptate eos eum!</p>
               </Panel>
               <Panel header="Apply Job" key="3" className="site-collapse-custom-panel">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, inventore nulla? Sed enim architecto ab corporis nihil eius tempore pariatur, iure eaque! Modi, porro ipsam.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, inventore nulla? Sed enim architecto ab corporis nihil eius tempore pariatur, porro ipsam.</p>
               </Panel>
             </Collapse>
           </div>
