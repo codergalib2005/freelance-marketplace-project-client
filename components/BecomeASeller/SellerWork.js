c059086066f0b1b8dca57b877c32264dda14f2c0
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-04-09 12:03:33 +0600

    something change like font size and position

--- components/BecomeASeller/SellerWork.js
+++ components/BecomeASeller/SellerWork.js
@@ -7,14 +7,14 @@ const SellerCommunity = () => {
       <div className="container-fluid mx-auto py-12 mt-20 text-center">
         <h2 className="text-4xl font-sans font-semibold mb-12">How It Works</h2>
         <div className="grid lg:grid-cols-3 md:grid-cols-1 md:mx-4 text-gray-400 gap-8">
-          <div className="shadow py-2 px-3 cursor-pointer hover:bg-white duration-300">
+          <div className=" py-2 px-3 cursor-pointer hover:bg-white duration-300">
             <div>
               <img className="block mx-auto" src="https://i.ibb.co/hc6Xh34/Screenshot-1.png" alt="" />
             </div>
             <h2 className="text-2xl text-gray-500">1. Create A Gig</h2>
             <p>Sign up for free, set up your Gig, and offer your work to our global audience.</p>
           </div>
-          <div className="shadow py-2 px-3 cursor-pointer hover:bg-white duration-300">
+          <div className=" py-2 px-3 cursor-pointer hover:bg-white duration-300">
             <div>
               <img className="block mx-auto" src="https://i.ibb.co/Hp44jfy/Screenshot-2.png" alt="" />
             </div>
@@ -24,7 +24,7 @@ const SellerCommunity = () => {
               Payment is{" "}
             </p>
           </div>
-          <div className="shadow py-2 px-3 cursor-pointer hover:bg-white duration-300">
+          <div className=" py-2 px-3 cursor-pointer hover:bg-white duration-300">
             <div>
               <img className="block mx-auto" src="https://i.ibb.co/qs42zzX/Screenshot-3.png" alt="" />
             </div>
