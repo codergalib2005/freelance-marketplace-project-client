cf5d0775f8e94183b8de0b8c7370020d3f87fb43
minhazpro3 <minhazpro1@gmail.com>
2022-04-11 23:45:39 +0600

    styled banner

--- pages/community/CommunityBanner.js
+++ pages/community/CommunityBanner.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const CommunityBanner = () => {
-    return (
-        <div>
-             <div className="bg-slate-200">
-              <div className="container-fluid m-0">
-              <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-                    <div className="">
-                        <h4>Welcome to the Freelance <span>Community</span></h4>
-                        <h5>Get access to upcoming events, exclusive content, the latest and greatest industry news, and more.</h5>
-                        <button className="bg-green-500 border-2 hover:bg-emerald-50 border-green-500 px-6 py-2 text-lg font-medium  transition-all duration-500 hover:text-green-500 text-white rounded-md">Sign up now</button>
-                    </div>
-                    <div className="">
-                         {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img className="w-full" src="/community/1_hero-artwork.jpg" alt="" />
-                    </div>
-                </div>
-              </div>
-             </div>
+  return (
+    <div>
+      <div style={{ backgroundColor: "#ffffff" }}>
+        <div className="">
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12 my-16 items-center">
+            <div className="px-20 ">
+              <h4 className="text-3xl md:text-4xl lg:text-6xl leading-loose		">
+                Welcome to the Freelance{" "}
+                <span className=" font-serif">Community</span>
+              </h4>
+              <h5 className=" text-xl md:text-lg lg:text-2xl my-8">
+                Get access to upcoming events, exclusive content, the latest and
+                greatest industry news, and more.
+              </h5>
+              <button className="bg-green-500 border-2 hover:bg-emerald-50 border-green-500 px-6 py-2 text-lg font-medium  transition-all duration-500 hover:text-green-500 text-white rounded-md ">
+                Sign up now
+              </button>
+            </div>
+            <div className=" flex justify-end">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/community/1_hero-artwork.jpg" alt="" />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CommunityBanner;
