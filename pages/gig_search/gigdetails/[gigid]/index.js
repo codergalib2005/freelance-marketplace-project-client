97cdd0ef44a7ff9ccefaf35626651188d2395f3d
codergalib20 <coderboygalif@gmail.com>
2022-04-13 01:08:00 +0600

    commited with details gig data load

--- pages/gig_search/gigdetails/[gigid]/index.js
+++ pages/gig_search/gigdetails/[gigid]/index.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import React from 'react';
 const GigDetails = () => {
+  // const [gig, setSingleGig]
   const router = useRouter()
-  console.log(router.query.gigid);
+  const GETURL = `https://dry-plains-53771.herokuapp.com/auth/gigs/${router?.query?.gigid}`
+  axios.get(GETURL)
+    .then(res => console.log(res?.data?.result[0]))
+    .catch(err => console.log(err));
   return (
     <div>
-      <h1>Gig Details {route.query.gigid}</h1>
+      <h1>Gig Details {router.query.gigid}</h1>
     </div>
   );
 };
