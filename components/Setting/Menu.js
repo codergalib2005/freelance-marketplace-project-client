4f799430ee77af0079ab0349a7f502d79ed060ac
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-04-12 15:56:51 +0600

    just setup of setting menu

--- components/Setting/Menu.js
+++ components/Setting/Menu.js
@@ -1,20 +1,44 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Menu = () => {
-    return (
-        <>
-                <nav className='grid place-items-stretch'>
-                    <ul>
-                        <Link><li>Account</li></Link>
-                        <Link><li>Security</li></Link>
-                        <Link><li>Notification</li></Link>
-                        <Link><li>Payment methods</li></Link>
-                        <Link><li>Form W-9</li></Link>
-                    </ul>
-                </nav>
-            </> 
-    );
+  return (
+    <>
+      <nav>
+        <ul className="grid grid-rows-4">
+          <li>
+            <Link href="/" passHref>
+              <a className="hover:text-green-400 text-lg py-4 px-6">Account</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="hover:text-green-400 text-lg py-4 px-6">Security</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="hover:text-green-400 text-lg py-4 px-6">
+                Notification
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="hover:text-green-400 text-lg py-4 px-6">
+                Payment methods
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="hover:text-green-400 text-lg py-4 px-6">Form W-9</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Menu;
---
+++ pages/menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "../components/Setting/Menu";
+
+const menu = () => {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+};
+
+export default menu;
