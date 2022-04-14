27f22bdf248b85785b646becc4faf55b677ff541
unknown <aarnob2020@gmail.com>
2022-04-15 00:25:29 +0600

    payment page done

--- components/Home/HowItWorks.js
+++ components/Home/HowItWorks.js
@@ -95,7 +95,6 @@ const HowItWorks = () => {
           {/* eslint-disable-next-line react/no-unknown-property */}
           <iframe className={`${styles.how_it_work_img} w-full`} height="380" src="https://www.youtube.com/embed/77g4KA9WQL4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-      </div>
     </section>
   );
 };
