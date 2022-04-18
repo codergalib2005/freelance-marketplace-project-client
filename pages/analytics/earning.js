31786ec87d3a6220c49019ff4ec472de0f984e33
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-04-18 19:32:43 +0600

    added new route

---
+++ pages/analytics/earning.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const earning = () => {
+  return (
+    <div>
+      <h3>Earning Our Marketplace</h3>
+    </div>
+  );
+};
+
+export default earning;
---
+++ pages/sellers/topSellers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const topSellers = () => {
+  return (
+    <div>
+      <h3>TOP Sellers</h3>
+    </div>
+  );
+};
+
+export default topSellers;
