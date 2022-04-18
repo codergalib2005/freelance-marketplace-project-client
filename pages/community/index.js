cc4b45ba89159183a74863a0c3c924160b5fc10a
minhazpro3 <minhazpro1@gmail.com>
2022-04-18 16:48:37 +0600

    added community banner

--- pages/community/index.js
+++ pages/community/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Refer from "../Refer/Refer";
 import CommunityBanner from "./CommunityBanner";
+import Banner from '../../components/Community/Banner'
 
 const Coummunity = () => {
   return (
     <div>
+      <Banner/>
       <CommunityBanner />
       <Refer />
     </div>
