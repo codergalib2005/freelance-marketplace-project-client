045c6f6a00823b82d14fbfd1b1453218d4be8bdc
unknown <aarnob2020@gmail.com>
2022-04-19 13:10:58 +0600

    error solve

--- pages/dashboard/admin.js
+++ pages/dashboard/admin.js
@@ -1,46 +1,68 @@
 import React, { useState } from 'react';
-import { Input } from 'antd';
+import { Alert, Input } from 'antd';
 import styles from '../../styles/payment.module.css';
+import axios from 'axios';
+
 
 const admin = () => {
-    const [email, setEmail] = useState('');
+    const [Temail, setEmail] = useState({});
+    const [adminSuccess, setAdminSuccess] = useState(false)
+    const [admin, setadmin] = useState(`${Temail?.email}`);
 
     const handleONBlur = e =>{
         setEmail(e.target?.value)
     }
 
     const handleAdminSubmit= e =>{
-        const user = {email}
-        fetch('', {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        .then(res => res.json())
-        .then(data =>{
-           console.log(data);
-        })
-        e.preventDefault()
+        // const user = {email}
+        // fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/admin/${email}`, {
+        //     method: 'PUT',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(user)
+        // })
+        // .then(res => res.json())
+        // .then(data =>{
+        //     if(data.modifiedCount){
+        //         console.log(data.result[0]);
+        //         setAdminSuccess(true)
+        //     }
+        // })
+
+        // if(!admin){
+        //     message.error("Admin Must need to fill-up!");
+        //     return;
+        // }else{
+
+        //     axios.put(`${process.env.NEXT_PUBLIC_API_URL}/users/admin/${Temail?.email}`, {
+        //         admin: admin
+        //     })
+        //     .then(res => {
+        //         console.log(res.result)
+        //         setAdminSuccess(true)
+        //     })
+        // }
+
+        // // e.preventDefault()
     }
     return (
         <div className='container-fluid mx-auto p-28 min-h-screen'>
         <div className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-12'>
             <div className={`flex justify-center ${styles.payment_container}`}>
                 <form onSubmit={handleAdminSubmit} >
-                    {/* <Input placeholder="Your Name"  /> */}
-                    <br />
+                    
                     <br />
                     <Input placeholder="Your Email" type="email" onBlur={handleONBlur} />
                     <br />
                     
-                    <div className="mt-8 flex justify-center">
-                        <button className="bg-green-500 text-white px-8 py-2 rounded-sm">
+                    {/* <div className="mt-8 flex justify-center"> */}
+                        <button type="submit" className="bg-green-500 text-white px-8 py-2 rounded-sm">
                             Make Admin
                         </button>
-                    </div>
+                    {/* </div> */}
                 </form>
+                {adminSuccess && <Alert message="Successfully created" type="success" />}
             </div>
             <div>
                 <img src="https://img.freepik.com/free-vector/top-up-credit-concept-illustration_114360-7284.jpg?size=338&ext=jpg&ga=GA1.1.1852109835.1638216464" alt="" />
@@ -51,7 +73,7 @@ const admin = () => {
 };
 
 export default admin;
-
+//admin create
 // app.put('users/admin', async (req, res) =>{
 //     const user = req.body;
 //     const filter = {emeil: user.email};
@@ -60,3 +82,16 @@ export default admin;
 //     res.json(result)
 
 // })
+
+
+//check admin
+// app.get('/users/:email', async (req, res) =>{
+//     const email =  req.params.email;
+//     const query  = {email: email};
+//     const user = await usersCollection.findOne(query);
+//     let isAdmin = false;
+//     if(user?.role === 'admin'){
+//         isAdmin= true
+//     }
+//     res.json9({admin: isAdmin});
+// } )
