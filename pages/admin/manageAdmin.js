2d7232e8370646a93ef3bc41c5a2fe973326f13f
unknown <aarnob2020@gmail.com>
2022-04-20 13:23:03 +0600

    commited

--- pages/admin/manageAdmin.js
+++ pages/admin/manageAdmin.js
@@ -6,57 +6,74 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Link from 'next/link';
+import styles from '../../styles/manageAdmin.module.css';
+import SideBar from "../../components/SideBar/SideBar";
+import SideBarMenu from "../../components/SideBar/SideBarMenu";
+import MainDashboard from "../../components/Dashboard/MainDashboard";
 
 
 
 const manageAdmin = () => {
   const [ourUsers, setOurUsers] = useState([]);
-  
-  
 
-  useEffect(() =>{
+
+
+  useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`)
-    .then(res => res.json())
-    .then(data => setOurUsers(data.result))
-  },[]);
+      .then(res => res.json())
+      .then(data => setOurUsers(data.result))
+  }, []);
 
- 
+
   return (
-    <TableContainer className="p-10" component={Paper} >
-      <Table  aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="left">Email</TableCell>
-            <TableCell align="left">Role</TableCell>
-            <TableCell align="left"></TableCell>
-            <TableCell align="left"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-       {
-         ourUsers.map(OurUser => (
-               
-          <TableRow
-            key={OurUser._id}
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          <TableCell component="th" scope="row">
-          {OurUser.name}
-          </TableCell>
-          <TableCell align="left">{OurUser.email}</TableCell>
-          <TableCell align="left">{OurUser.admin}</TableCell>
-          <TableCell align="left"><button >Make Admin</button></TableCell>
-          <TableCell align="left"><button >Remove Admin</button></TableCell>
-        </TableRow>
-         ))
-       }
-      
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <section>
+    <main>
+      <div className="dashboard_position bg-[#00073d]">
+        <div>
+          {" "}
+          <SideBar />
+          <SideBarMenu />
+        </div>
+        <div className="">
+        <TableContainer className={styles.table_container} component={Paper} >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="left">Role</TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              ourUsers.map(OurUser => (
+
+                <TableRow
+                  key={OurUser._id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {OurUser.name}
+                  </TableCell>
+                  <TableCell align="left">{OurUser.email}</TableCell>
+                  <TableCell align="left">{OurUser.admin}</TableCell>
+                  <TableCell align="left"><Link href={'/admin/makeAdmin'}><button className={styles.admin_button}>Make Admin</button></Link></TableCell>
+                  <TableCell align="left"><Link href={'/admin/removeAdmin'}><button >Remove Admin</button></Link></TableCell>
+                </TableRow>
+              ))
+            }
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+        </div>
+      </div>
+    </main>
+  </section>
   );
 };
 
 export default manageAdmin;
-
