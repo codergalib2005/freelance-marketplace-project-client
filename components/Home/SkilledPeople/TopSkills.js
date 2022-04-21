80bb5d2caa88b8aadc3d09b70797ee02db9a7f62
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-04-22 01:06:46 +0600

    skills tab update and menu drawer  setup

--- components/Home/SkilledPeople/TopSkills.js
+++ components/Home/SkilledPeople/TopSkills.js
@@ -8,41 +8,28 @@ const TopSkills = () => {
           <div>
             <ul>
               <li>Blockchain</li>
-              <li>Go development</li>
               <li>Node.js</li>
               <li>Vue.js</li>
               <li>HR consulting</li>
               <li>Microsoft Power BI</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
               <li>Instructional design</li>
               <li>React.js</li>
               <li>Videographers</li>
               <li>HTML5 Developers</li>
               <li>Ghostwriters</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
               <li>Unity 3D Developers</li>
               <li>Business Consultants</li>
               <li>Coders</li>
               <li>Marketing Consultants</li>
               <li>Web Developers</li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>JavaScript Developer</li>
-              <li>Logo Designer</li>
-              <li>Mobile App Developer</li>
-              <li>PHP Developer</li>
-              <li>Python Developer</li>
-              <li>Resume Writer</li>
-              <li>SEO Expert</li>
-              <li>React.js</li>
-              <li>Social Media Manager</li>
-              <li>Software Developer</li>
-              <li>Ghostwriters</li>
-              <li>Software Engineer</li>
-              <li>Technical Writer</li>
-              <li>UI Designer</li>
-              <li>UX Designer</li>
-              <li>Virtual Assistant</li>
             </ul>
           </div>
         </div>
---
+++ components/Setting/MenuDrawer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+
+const MenuDrawer = () => {
+  return (
+    <>
+      <Drawer>
+        <div>
+          <Typography></Typography>
+        </div>
+      </Drawer>
+    </>
+  );
+};
+
+export default MenuDrawer;
