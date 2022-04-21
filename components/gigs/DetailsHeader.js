0ff21186963493bbc41ce800fae0055bec9e744f
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-04-22 05:43:01 +0600

    simple update of gig details page

--- components/gigs/DetailsHeader.js
+++ components/gigs/DetailsHeader.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import Header from "../Shared/Header";
 
 const DetailsHeader = () => {
   return (
-    <div className='bg-[#0a1929] border-b-2 borer py-2'>
-      <div className='container-fluid mx-auto px-3'>Header</div>
+    <div className="bg-[#0a1929] border-b-2 borer py-2">
+      <div className="container-fluid mx-auto px-3">
+        <Header />
+      </div>
     </div>
   );
 };
---
+++ pages/drawer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuDrawer from "../../components/Setting/MenuDrawer";
+
+const index = () => {
+  return (
+    <div>
+      <MenuDrawer />
+    </div>
+  );
+};
+
+export default index;
