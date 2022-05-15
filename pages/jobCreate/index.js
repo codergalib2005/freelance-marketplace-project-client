2b4444b31ac3af648c3412dec1c65a511895cf83
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-05-15 19:29:05 +0600

    commited

--- pages/jobCreate/index.js
+++ pages/jobCreate/index.js
@@ -42,8 +42,8 @@ const JobCreate = () => {
     technologies.push(data.technology4);
     technologies.push(data.technology5);
     technologies.push(data.technology6);
-    body.technologies = data.technologies;
-    body.companyDescription = data.JobCreatecompanyDescription;
+    body.technologies = technologies;
+    body.companyDescription = data.companyDescription;
     body.companyName = data.companyName;
     body.email = data.email;
     body.firstName = data.firstName;
@@ -52,7 +52,9 @@ const JobCreate = () => {
     body.jobType = data.jobType;
     body.lastName = data.lastName;
     body.role = data.role;
+    body.salary = data.salary
     console.log(body);
+    console.log(data);
   };
   return (
     <>
@@ -240,13 +242,13 @@ const JobCreate = () => {
                     style={{ boxShadow: "-2px 3px 15px rgba(0,0,0,0.05)" }}
                     type="text"
                     className="border-[2px] px-2 rounded-2 py-2 focus:outline-none rounded-md w-[90%] ml-8"
-                    {...register("jobTitle")}
+                    {...register("salary")}
                   >
                     <option value=""></option>
-                    <option value="A">Under 15k</option>
-                    <option value="B">20k - 30k</option>
-                    <option value="C">30k - 40k</option>
-                    <option value="D">40k +</option>
+                    <option value="Under 15k">Under 15k</option>
+                    <option value="20k - 30k">20k - 30k</option>
+                    <option value="30k - 40k">30k - 40k</option>
+                    <option value="40k +">40k +</option>
                   </select>
                 </div>
               </div>{" "}
