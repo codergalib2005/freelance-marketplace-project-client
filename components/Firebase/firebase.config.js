e4c58a279927b947b6a9b3e071e351c847c736dd
Asadullah Hil Galib <86350811+codergalib20@users.noreply.github.com>
2022-05-18 11:20:21 +0600

    commited

--- components/Firebase/firebase.config.js
+++ components/Firebase/firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDMVcd6cQQrb2ks2OIZJhBWjuWWTppn2dM",
-  authDomain: "freelance-marketplace-4ba35.firebaseapp.com",
-  projectId: "freelance-marketplace-4ba35",
-  storageBucket: "freelance-marketplace-4ba35.appspot.com",
-  messagingSenderId: "786019277760",
-  appId: "1:786019277760:web:ca2036089b958e56494625",
+  apiKey: "AIzaSyB-a9Jr_ru3DerAyz8FWNPO2fxq9qhJ6iI",
+  authDomain: "freelance-marketplace-project.firebaseapp.com",
+  projectId: "freelance-marketplace-project",
+  storageBucket: "freelance-marketplace-project.appspot.com",
+  messagingSenderId: "1019456019601",
+  appId: "1:1019456019601:web:c135f59739d860c69bb330",
+  measurementId: "G-1VXH7YXZMY",
 };
 
 export default firebaseConfig;
